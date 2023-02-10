849e55f98472021921a27adf43506d8f82020b6f
Darshanp30 <91684516+Darshanp30@users.noreply.github.com>
2023-02-10 23:27:22 +0530

    Delete j01.java

--- j01.java
+++
@@ -1,7 +0,0 @@
-//Write a Java program to print your introduction on screen.
-public class j01{
-    public static void main(String[] args){
-        System.out.println("My name is Darshan Patil from Indore M.P and enrolled for java zero to hero program on Let's Upgrade plateform");
-    
-    }
-}
